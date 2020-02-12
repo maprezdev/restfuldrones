26810c731f2a42d680a584ec15fa51826cb4df76
maprezdev <perezdhoz@gmail.com>
2020-02-12 16:34:23 -0500

    Update restfuldrones/urls.py file

--- restfuldrones/urls.py
+++ restfuldrones/urls.py
@@ -18,6 +18,6 @@ Including another URLconf
 
 from django.conf.urls import url, include
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls')),
     url(r'^', include('drones.urls')),
 ]
